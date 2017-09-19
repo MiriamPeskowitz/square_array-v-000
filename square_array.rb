9500d9e1c2e88d3ed290e62501c818907439af80
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-19 21:07:23 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,6 +2,7 @@
 
 def square_array(array)
     new_square = []
-    array.each { |num| new_square << num ** 2 }
+    #array.each { |num| new_square << num ** 2 }
+    array.each { |num| new_square.push(num ** 2)}
     new_square
 end
